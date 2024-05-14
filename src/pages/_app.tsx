5289c0321bd2a7674e3250fbf73db1c41bfe0b76
AJAY KUMAR <66991625+kumarajay0412@users.noreply.github.com>
2024-05-14 20:43:14 +0530

    vercel analytics and speed insights added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
 import Footer from "@/components/footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <div className=" p-3 h-screen w-screen  flex justify-start items-center flex-col  overflow-scroll">
+      <SpeedInsights />
+      <Analytics />
       <Header />
       <div className="">
         <AnimatePresence mode="wait" initial={false}>
