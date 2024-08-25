99ec80f93c8577058d36c1bf86e3c3024960ae78
AJAY KUMAR <66991625+kumarajay0412@users.noreply.github.com>
2024-08-26 01:58:17 +0530

    add rewrites for posthog

--- next.config.js
+++ next.config.js
@@ -10,6 +10,22 @@ const nextConfig = {
         };
         return config;
     },
+    async rewrites() {
+        return [
+            {
+                source: '/ingest/static/:path*',
+                destination: 'https://us-assets.i.posthog.com/static/:path*'
+            },
+            {
+                source: '/ingest/:path*',
+                destination: 'https://us.i.posthog.com/:path*'
+            },
+            {
+                source: '/ingest/decide',
+                destination: 'https://us.i.posthog.com/decide'
+            }
+        ];
+    },
 };
 
 module.exports = nextConfig;
