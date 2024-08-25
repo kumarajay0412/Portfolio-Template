3854c3894c149deb697d38cfc61faec033c6dc67
AJAY KUMAR <66991625+kumarajay0412@users.noreply.github.com>
2024-08-26 01:46:46 +0530

    feat add posthog analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,22 @@ import { AnimatePresence } from "framer-motion";
 import Footer from "@/components/footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+if (typeof window !== "undefined") {
+  // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host:
+      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+    },
+  });
+}
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
@@ -18,11 +30,13 @@ export default function App({ Component, pageProps, router }: AppProps) {
       <SpeedInsights />
       <Analytics />
       <Header />
-      <div className="">
-        <AnimatePresence mode="wait" initial={false}>
-          <Component {...pageProps} key={router.asPath} />
-        </AnimatePresence>
-      </div>
+      <PostHogProvider client={posthog}>
+        <div className="">
+          <AnimatePresence mode="wait" initial={false}>
+            <Component {...pageProps} key={router.asPath} />
+          </AnimatePresence>
+        </div>
+      </PostHogProvider>
       <Footer />
     </main>
   );
