c0b14c5fb9445b3a74487b38136100431951c7f0
AJAY KUMAR <66991625+kumarajay0412@users.noreply.github.com>
2024-08-26 01:50:54 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 if (typeof window !== "undefined") {
   // checks that we are client-side
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
     api_host:
       process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
