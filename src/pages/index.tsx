67cfef3af1707c2629dea61840bc0ebde15eaa6b
AJAY KUMAR <66991625+kumarajay0412@users.noreply.github.com>
2025-02-10 23:57:16 +0530

    update Banner heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,16 +14,14 @@ export default function Page() {
     <Layout>
       <div className="pt-8 max-w-3xl w-full h-full">
         <div className="text-gray-500">
-          I&apos;m a Software Engineer currently working at{" "}
+          I&apos;m a Software Engineer passionate about building high-performance web and mobile applications. I love crafting beautiful and scalable digital experiences, whether it's React web apps or React Native mobile apps. I actively contribute to open-source projects like Plate Previously, I worked with{" "}
           <a
             className="hover:text-gray-600 cursor-pointer underline"
             href="https://gan.ai/"
           >
             Gan.ai
           </a>
-          , where I work on building the next generation of video
-          personalization platform. I love to craft beautiful web products.
-          Previously, I have worked with amazing companies like{" "}
+          ,{" "}
           <a
             className="hover:text-gray-600 cursor-pointer underline"
             href="https://www.mercari.com/"
@@ -67,7 +65,7 @@ export default function Page() {
                   Delhi, India
                 </span>
                 <br />
-                <div className="text-xs mt-2 ">Jan 2023 - Present</div>
+                <div className="text-xs mt-2 ">Jan 2023 - Nov 2024</div>
               </div>
             </div>
           </li>
@@ -239,44 +237,7 @@ export default function Page() {
               Personal Portfolio website and Blogging Platform
             </div>
           </motion.div>
-          <motion.div
-            onClick={() =>
-              window.open("https://github.com/kumarajay0412/Clear-UI", "_blank")
-            }
-            whileHover={{
-              y: -10, // Move up by 10 pixels
-              transition: {
-                type: "spring",
-                stiffness: 300,
-                damping: 10,
-              },
-            }}
-            whileTap={{ scale: 0.95 }}
-            className="w-full h-full  rounded-md p-3 flex gap-4 flex-col cursor-pointer "
-          >
-            <div className=" h-8 w-8 p-2 rounded-md bg-black ">
-              <svg
-                width="15"
-                height="15"
-                className="text-white"
-                viewBox="0 0 15 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M0.877075 7.49985C0.877075 3.84216 3.84222 0.877014 7.49991 0.877014C11.1576 0.877014 14.1227 3.84216 14.1227 7.49985C14.1227 11.1575 11.1576 14.1227 7.49991 14.1227C3.84222 14.1227 0.877075 11.1575 0.877075 7.49985ZM3.78135 3.21565C4.68298 2.43239 5.83429 1.92904 7.09998 1.84089V6.53429L3.78135 3.21565ZM3.21567 3.78134C2.43242 4.68298 1.92909 5.83428 1.84095 7.09997H6.5343L3.21567 3.78134ZM6.5343 7.89997H1.84097C1.92916 9.16562 2.43253 10.3169 3.21579 11.2185L6.5343 7.89997ZM3.78149 11.7842C4.6831 12.5673 5.83435 13.0707 7.09998 13.1588V8.46566L3.78149 11.7842ZM7.89998 8.46566V13.1588C9.16559 13.0706 10.3168 12.5673 11.2184 11.7841L7.89998 8.46566ZM11.7841 11.2184C12.5673 10.3168 13.0707 9.16558 13.1588 7.89997H8.46567L11.7841 11.2184ZM8.46567 7.09997H13.1589C13.0707 5.83432 12.5674 4.68305 11.7842 3.78143L8.46567 7.09997ZM11.2185 3.21573C10.3169 2.43246 9.16565 1.92909 7.89998 1.8409V6.53429L11.2185 3.21573Z"
-                  fill="currentColor"
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </div>
-            <div className="text-md text-gray-600">Clear UI</div>
-            <div className="text-xs text-gray-500">
-              A list of components with great animation and ready to use in
-              production
-            </div>
-          </motion.div>
+
           <motion.div
             onClick={() => window.open("https://chartr.in/", "_blank")}
             whileHover={{
